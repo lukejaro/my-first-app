bbcdb8cdce5b21488eb9de7d554de8f2ca3557db
= <=>
2020-01-05 18:30:33 -0600

    Done for the day

---
+++ just-for-fun/src/Components/Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Table extends React.Component{
+    // fill in rows while there is data
+    // expect user to pass in data 
+
+    render() {
+        return (
+            <table>
+                <tr>Hey</tr>
+             </table> 
+        );
+        
+    }
+}
+export default Table;
